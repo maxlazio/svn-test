261f1853d328b7c700ce05577f6f2db7083ff9ed
nbrookins <nbrookins@3fb922f0-06aa-47c5-b065-122db1738c5e>
2008-12-16 20:48:33 +0000

    Planned update for SmoothHD.com, still in progress.

--- silverlight/MediaFrameworkTests/Properties/AssemblyInfo.cs
+++ silverlight/MediaFrameworkTests/Properties/AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Runtime.InteropServices;
 [assembly: Guid("93ae569c-e783-47d3-bccc-e942b508cf3d")]
 
 //NOTE: Automatic Versioning - from version.txt.  Do not modify the version directly.
-[assembly: AssemblyVersion("2.0.30.22")]
-[assembly: AssemblyFileVersion("2.0.30.22")]
+[assembly: AssemblyVersion("2.0.30.21")]
+[assembly: AssemblyFileVersion("2.0.30.21")]
