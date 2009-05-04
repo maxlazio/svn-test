f7b538c4edefb9b834bf64ac4458821d0a44d022
nbrookins <nbrookins@3fb922f0-06aa-47c5-b065-122db1738c5e>
2009-05-04 17:58:13 +0000

    Resync for v2.2

--- silverlight/OVPWeb/Properties/AssemblyInfo.cs
+++ silverlight/OVPWeb/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("OpenVideoPlayer.com")]
 [assembly: AssemblyProduct("OVPWeb")]
-[assembly: AssemblyCopyright("Copyright OpenVideoPlayer.com 2008")]
+[assembly: AssemblyCopyright("(c) 2008-2009 OpenVideoPlayer")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("3d5900ae-111a-45be-96b3-d9e4606ca793")]
 
 //NOTE: Automatic Versioning - from version.txt.  Do not modify the version directly.
-[assembly: AssemblyFileVersion("2.2.0.60")]
-[assembly: AssemblyVersion("2.2.0.60")]
+[assembly: AssemblyFileVersion("2.2.0.64")]
+[assembly: AssemblyVersion("2.2.0.64")]
